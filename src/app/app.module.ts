3b9a47e3562effccdc8ab540bb7135b2b79f1a27
Clay <clay@dryadmedia.com>
2022-01-28 13:42:35 -0600

    Generate photo service & photo-button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PhotoButtonComponent } from './photo-button/photo-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoButtonComponent
   ],
   imports: [
     BrowserModule
